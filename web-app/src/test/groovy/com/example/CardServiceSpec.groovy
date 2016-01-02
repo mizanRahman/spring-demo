9bc70ce52c4c7b98ef3c2f1ccab2f459b8de6b12
Mizan Rahman <mizan.cse.buet@gmail.com>
2016-01-02 14:57:55 +0600

    temporary commit

---
+++ web-app/src/test/groovy/com/example/CardServiceSpec.groovy
@@ -0,0 +1,18 @@
+package com.example
+
+import com.example.core.repository.CardRepository
+import com.example.core.service.CardService
+import com.example.core.service.CardServiceImpl
+import org.springframework.beans.factory.annotation.Autowired
+import spock.lang.Specification
+
+/**
+ * Created by mac on 1/2/16.
+ */
+class CardServiceSpec extends Specification{
+
+
+    def "saves card"() {
+
+    }
+}
