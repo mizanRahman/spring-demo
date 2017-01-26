5ead7200fa8871200fa4f7dba8ae20557dfd2f7c
Mizan Rahman <mizan.cse.buet@gmail.com>
2017-01-27 02:26:46 +0600

    ok

--- web-app/src/test/java/com/example/RestAssuredSetupRule.java
+++ web-app/src/test/java/com/example/RestAssuredSetupRule.java
@@ -1,15 +1,16 @@
 package com.example;
 
 import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.config.RestAssuredConfig;
+import com.jayway.restassured.builder.RequestSpecBuilder;
+import com.jayway.restassured.config.MatcherConfig;
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.rules.ExternalResource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.util.UUID;
 
 /**
  * Created by mizan on 1/19/17.
@@ -26,21 +27,28 @@ public class RestAssuredSetupRule extends ExternalResource {
 
 
 
+
     @Override
     protected void before() {
         MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
         RestAssured.port = port;
         RestAssuredMockMvc.mockMvc(mockMvc);
 
-        // logging concern
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-        // json request
-        RestAssured.requestSpecification.accept(ContentType.JSON).contentType(ContentType.JSON);
+
+        RestAssured.requestSpecification =
+                new RequestSpecBuilder().setContentType(ContentType.JSON)
+                        .setAccept(ContentType.JSON)
+                        .setBasePath("/api")
+                        .addHeader("X-Correlation_Id", UUID.randomUUID().toString())
+                        .build();
+
+        MatcherConfig matcherConfig = RestAssured.config().getMatcherConfig()
+                .errorDescriptionType(MatcherConfig.ErrorDescriptionType.HAMCREST);
+        RestAssured.config().matcherConfig(matcherConfig);
 
 
     }
-
-
 
 
 }
